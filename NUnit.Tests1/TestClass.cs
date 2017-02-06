58ab533d80fd6f96ec5fb26fb89ce9ce6cc46bee
Mueez Aizaz <mueezaizaz@hotmail.com>
2017-02-06 16:51:59 +0100

    Add project files.

---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            int numerator = 20;
+            int denominator = 5;
+            int expected = 4;
+
+            int result = TestingNunit.Class1.Divide(numerator, denominator);
+
+            Assert.AreEqual(expected,result);
+        
+            Assert.Pass("Your first passing test");
+            
+       
+        }
+    }
+}
---
+++ TestingNunit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingNunit
+{
+   public class Class1
+    {
+        public static int Divide(int numerator, int denominator)
+        {
+
+            int result = numerator / denominator;
+            return result;
+
+        }
+    }
+}
